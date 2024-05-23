71584288ed6c9697a95b0457df48c9710e8057bc
BrodenFrailingson <Brodiefrail@hotmail.com>
2024-05-23 02:34:43 +0000

    made camera stick to edges of world when at edges, camera now doesn't leave world bounds when at edges

--- Necromousey/Assets/Scrtips/FollowCamera.cs
+++ Necromousey/Assets/Scrtips/FollowCamera.cs
@@ -3,13 +3,40 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
    
-    public Transform Player;
-    public Vector3 OffSet;
+    [SerializeField] private Transform Player;
+    [SerializeField] private Vector3 OffSet;
+    
+    private Camera m_Camera;
 
+    [Header("World Bounds")]
+    //
+    // Summary:
+    //     distance form centre of map to outer edges.    
+    [SerializeField] private Vector2 Bounds;
 
+    void Start()
+    {
+        m_Camera = Camera.main;
+
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Player.position + OffSet;
+        Vector3 temppos = new();
+        Vector3 newpos = new();
+        temppos = Player.position + OffSet;
+        TestWorldBounds(temppos, out newpos);
+        transform.position = newpos;
+    }
+
+    private void TestWorldBounds(Vector3 pos, out Vector3 newpos)
+    {
+        float cameraWidth = m_Camera.orthographicSize * m_Camera.aspect;
+        if(pos.y + m_Camera.orthographicSize >=Bounds.y)    {pos.y = Bounds.y - m_Camera.orthographicSize; }
+        if(pos.y - m_Camera.orthographicSize <=-Bounds.y)  {pos.y = -Bounds.y  + m_Camera.orthographicSize; }
+        if(pos.x - cameraWidth <=-Bounds.x)  {pos.x = -Bounds.x  + cameraWidth;  }
+        if(pos.x + cameraWidth >= Bounds.x) {pos.x = Bounds.x - cameraWidth;  }
+        
+        newpos = pos;
     }
 }
