99017332dd0cc5052770089d67ae02c738b4a1f1
DGrant100 <davidgrant13@hotmail.co.uk>
2024-05-26 04:03:59 +0100

    Wave Spawner system
    The wave spawner system works for 2 round before breaking. Also, the mouse will start rotating after getting hit by enemy.

--- Necromousey/Assets/Scrtips/EnemyAI.cs
+++ Necromousey/Assets/Scrtips/EnemyAI.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,7 +5,11 @@ public class EnemyAI : MonoBehaviour
 {
     private float speed = 3f; 
     private Transform target;
-    // Start is called before the first frame update
+
+    public GameObject enemyPrefab;
+    public int cost;
+
+     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -27,8 +30,17 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 
+    void OnDestroy()
+    {
+        if (GameObject.FindGameObjectWithTag("WaveSpawner") != null)
+        {
+            GameObject.FindGameObjectWithTag("WaveSpawner").GetComponent<WaveSpawner>().spawnedEnemies.Remove(gameObject);
+        }
+    }
+
+   
     
 }
